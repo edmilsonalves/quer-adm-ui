8424d904e0a7894cb3f907ab2c87c5fff9cf8924
Edmilson Alves dos Reis <edmilson@univision.net.br>
2020-01-03 15:40:26 -0300

    dev cadastro de questionarios

--- src/app/pages/app-quer/questionario/questionario.component.ts
+++ src/app/pages/app-quer/questionario/questionario.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'sv-questionario',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionarioComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
+
+  editing = {};
+  rows = [];    
+  temp = [];
+  selected = [];
+
+  columns = [
+      { prop: 'codSingular' },
+      { name: 'nome' },
+      { name: 'categoria' },
+      { name: 'situacao' },
+      { name: 'criadoEm' },
+      { name: 'alteradoEm' }
+  ];
+  @ViewChild(DatatableComponent, {static: false}) table: DatatableComponent;
+
+  constructor() {
+    this.fetch((data) => {
+      this.temp = [...data];
+      this.rows = data;
+    });
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/quer.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  updateValue(event, cell, rowIndex) {
+      console.log('inline editing rowIndex', rowIndex)
+      this.editing[rowIndex + '-' + cell] = false;
+      this.rows[rowIndex][cell] = event.target.value;
+      this.rows = [...this.rows];
+      console.log('UPDATED!', this.rows[rowIndex][cell]);
+  }
+
+  updateFilter(event) {
+      const val = event.target.value.toLowerCase();
+      const temp = this.temp.filter(function(d) {
+          return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+      });
+      this.rows = temp;
+      this.table.offset = 0;
+  }
+
+  onSelect({ selected }) {
+      console.log('Select Event', selected, this.selected);
+      this.selected.splice(0, this.selected.length);
+      this.selected.push(...selected);
+  }
+
+  onActivate(event) {
+      console.log('Activate Event', event);
+  }
+  
 
 }
